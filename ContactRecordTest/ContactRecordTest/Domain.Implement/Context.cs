aff46fa45cf1e702042ccf155f85fcefa687f9b3
Pablo Mendez <mendez.developer@gmail.com>
2018-12-10 21:33:52 -0300

    added context

---
+++ ContactRecordTest/ContactRecordTest/Domain.Implement/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactRecordTest.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ContactRecordTest.Domain.Implement
+{
+    public class Context : IContext
+    {
+        private readonly IMongoDatabase db;
+
+        public Context(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            db = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Contact> Contacts => db.GetCollection<Contact>("Contacts");
+    }
+}
---
+++ ContactRecordTest/ContactRecordTest/Domain/IContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactRecordTest.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactRecordTest.Domain
+{
+    public interface IContext
+    {
+        IMongoCollection<Contact> Contacts { get; }
+    }
+}
