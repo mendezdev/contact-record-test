be813cc76fbc6b64fa44def3370cdc4603cc3a08
Pablo Mendez <mendez.developer@gmail.com>
2018-12-10 21:50:57 -0300

    added contact domain logic

---
+++ ContactRecordTest/ContactRecordTest/Domain.Implement/ContactDomain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactRecordTest.Models;
+using MongoDB.Driver;
+
+namespace ContactRecordTest.Domain.Implement
+{
+    public class ContactDomain : IContactDomain
+    {
+        private readonly IContext context;
+
+        public ContactDomain(IContext context)
+        {
+            this.context = context;
+        }
+
+        public Task Create(Contact contact)
+        {
+            return context.Contacts.InsertOneAsync(contact);
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            DeleteResult deleteResult = await context
+                .Contacts.DeleteOneAsync(
+                    filter: c => c.Id == id
+                );
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<IEnumerable<Contact>> GetAllContacts()
+        {
+            return await context.Contacts.Find(_ => true).ToListAsync();
+        }
+
+        public Task<Contact> GetContactById(string id)
+        {
+            FilterDefinition<Contact> filter = Builders<Contact>.Filter.Eq(m => m.Id, id);
+            return context.Contacts.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> Update(Contact contact)
+        {
+            ReplaceOneResult updateResult = await context
+                .Contacts.ReplaceOneAsync(
+                    filter: c => c.Id == contact.Id,
+                    replacement: contact);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
---
+++ ContactRecordTest/ContactRecordTest/Domain/IContactDomain.cs
@@ -0,0 +1,17 @@
+﻿using ContactRecordTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactRecordTest.Domain
+{
+    public interface IContactDomain
+    {
+        Task<IEnumerable<Contact>> GetAllContacts();
+        Task<Contact> GetContactById(string id);
+        Task Create(Contact contact);
+        Task<bool> Update(Contact contact);
+        Task<bool> Delete(string id);
+    }
+}
