6d9bd7b9e31813642a756deed44572175e9fbb0d
Pablo Mendez <mendez.developer@gmail.com>
2018-12-10 21:29:59 -0300

    added Contact class (in progress)

---
+++ ContactRecordTest/ContactRecordTest/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactRecordTest.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        public string ProfileImageUri { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
