4c7ec6b5d6966e691bd3dfb32ad1a9464a8d91a4
Pablo Mendez <mendez.developer@gmail.com>
2018-12-10 22:03:07 -0300

    added Contacts API (in progress)

---
+++ ContactRecordTest/ContactRecordTest/Controllers/ContactsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactRecordTest.Domain;
+using ContactRecordTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactRecordTest.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Contacts")]
+    public class ContactsController : Controller
+    {
+        private readonly IContactDomain contactDomain;
+
+        public ContactsController(IContactDomain contactDomain)
+        {
+            this.contactDomain = contactDomain;
+        }
+
+        // GET: api/Contacts
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await contactDomain.GetAllContacts());
+        }
+        
+        // POST: api/Contacts
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Contact contact)
+        {
+            await contactDomain.Create(contact);
+            return Ok();
+        }               
+    }
+}
